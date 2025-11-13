1b874f5cc80df430ecc01943dd7578aadc455c4b
Lucas Marques de Oliveira <luquinhaspsp@gmail.com>
2025-11-13 20:53:58 -0300

    Criação das classes CLiente e Conta, e criação dos métodos depositar e sacar

---
+++ ProjetoRevisaoPOO/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoRevisaoPOO
+{
+    abstract class Conta
+    {
+        public double  saldo { get; protected set; }
+        public int numeroConta { get; set; }
+        public int agencia { get; set; }
+        public Cliente cliente { get; set; }
+
+        protected Conta(int numeroConta, int agencia, Cliente cliente)
+        {
+            this.numeroConta = numeroConta;
+            this.agencia = agencia;
+            this.cliente = cliente;
+        }
+
+        public void depositar(double quantidade)
+        {
+            this.saldo += quantidade;
+        }
+        public void sacar(double quantidade)
+        {
+            this.saldo -= quantidade;
+        }
+
+    }
+
+
+
+
+
+}
